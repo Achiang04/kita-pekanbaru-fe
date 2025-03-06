aea609c8a695319b46dc154a67b03b4c766f2fd2
jecky hindrawan <jecky@coconutlab.io>
2025-03-07 01:13:05 +0700

    tweak: add transform response data

--- services/media/index.ts
+++ services/media/index.ts
@@ -2,12 +2,14 @@ import { api } from "../api";
 
 export const mediasApi = api.injectEndpoints({
   endpoints: (build) => ({
+    // TODO: please update any with proper TS type
     getNotifications: build.query<any, undefined>({
       query: () => ({
         url: "api/v1/medias",
         method: "GET",
       }),
       providesTags: ["Medias"],
+      transformResponse: (response: any) => response.data,
     }),
   }),
   overrideExisting: true,
